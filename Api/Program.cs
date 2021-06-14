144ae874a2089e6b1d9a542c777bf06e9ba6da2d
Ryan Hutson <ryan.hutson@americaneagle.com>
2021-06-14 12:51:58 -0500

    Add project files.

---
+++ Api/Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+using System;
+using System.Threading.Tasks;
+
+namespace Api
+{
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        CreateHostBuilder(args).Build().Run();
+    //    }
+
+    //    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    //        Host.CreateDefaultBuilder(args)
+    //            .ConfigureWebHostDefaults(webBuilder =>
+    //            {
+    //                webBuilder.UseStartup<Startup>();
+    //            });
+    //}
+
+    public class Program  // Auto run migrations if there are new ones. Pretty cool
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+
+                await Seed.SeedData(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration");
+            }
+
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
