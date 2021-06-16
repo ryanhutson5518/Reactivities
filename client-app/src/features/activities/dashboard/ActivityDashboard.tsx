468133eabab62847e8f590227244c388f11ddcf8
Ryan Hutson <ryan.hutson@americaneagle.com>
2021-06-16 15:16:54 -0500

    End of section 5

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,19 +3,43 @@ import { Grid } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
 import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
 
 interface Props {
     activities: Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityDashboard({ activities }: Props) {   // Destructure this object in order to get type safety
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) {   // Destructure this object in order to get type safety
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} />
+                <ActivityList
+                    activities={activities}
+                    selectActivity={selectActivity}
+                    deleteActivity={deleteActivity}
+                />
             </Grid.Column>
             <Grid.Column width='6'>
-                <ActivityDetails activity={activities[0]} />
+                {selectedActivity && !editMode &&
+                    <ActivityDetails
+                        activity={selectedActivity}
+                        cancelSelectActivity={cancelSelectActivity}
+                        openForm={openForm}
+                    />}   {/* && = Checks if activities[0] has a value. If it does, display the code */}
+                {editMode &&
+                    <ActivityForm
+                        activity={selectedActivity}
+                        closeForm={closeForm}
+                        createOrEdit={createOrEdit}
+                    />}
             </Grid.Column>
         </Grid>
     );
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -4,11 +4,13 @@ import { Activity } from '../../../app/models/activity';
 
 interface Props {
     activity: Activity
+    cancelSelectActivity: () => void;
+    openForm: (id: string) => void;
 }
 
-export default function ActivityDetails({ activity }: Props) {
+export default function ActivityDetails({ activity, cancelSelectActivity, openForm }: Props) {
     return (
-        <Card>
+        <Card fluid>
             <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
             <Card.Content>
                 <Card.Header>{activity.title}</Card.Header>
@@ -20,8 +22,8 @@ export default function ActivityDetails({ activity }: Props) {
                 </Card.Description>
                 <Card.Content extra>
                     <Button.Group widths='2'>
-                        <Button basic color='blue' content='Edit' />
-                        <Button basic color='grey' content='Cancel' />
+                        <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit' />
+                        <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel' />
                     </Button.Group>
                 </Card.Content>
             </Card.Content>
